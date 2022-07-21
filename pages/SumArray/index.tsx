b22d786652fc11ac669dd723d1812814ec4694d8
Daniel <dacamoty@gmail.com>
2022-07-20 23:26:56 -0500

    Change of nomenclature understandable in Exercise 1

--- pages/SumArray/index.tsx
+++ pages/SumArray/index.tsx
@@ -2,9 +2,10 @@ import { Layout } from "../../layouts/Layout";
 import { Grid, Text } from "@nextui-org/react";
 
 const SumArray = () => {
-  const sum = (a: number[]) => {
-    a = a.map((num: any) => (num > 20 && num % 2 === 0 ? 20 : num));
-    return (b: any) => b(a.reduce((prev: number, curr: number) => prev + curr));
+  const sum = (number: number[]) => {
+    number = number.map((num) => (num > 20 && num % 2 === 0 ? 20 : num));
+    return (callback: any) =>
+    callback(number.reduce((prev: number, curr: number) => prev + curr));
   };
   sum([1, 2, 3, 22, 21, 24, 27])((result: number) => console.log(result));
   return (
